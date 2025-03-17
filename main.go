649ec897e4a0d2b8333a99c26b9d26e10724fa8b
fog66 <pol13112005@gmail.com>
2025-03-17 16:29:59 +0300

    GitHub actions interactive
    Add ci.yml for autobuilding and making docker_image

--- main.go
+++ main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"search-engine/pkg/indexer"
 	"search-engine/pkg/parser"
 )
 
 func main() {
-	err := parser.StopWordsHandle.InitializeWordMap("../pkg/parser/utils/english_stop_words.txt")
-	if err != nil {
-		panic("Cannot scan stop-words")
+	english_stop_words_path := os.Getenv("ENGLISH_STOP_WORDS_PATH")
+	if english_stop_words_path == "" {
+		english_stop_words_path = "../pkg/parser/utils/english_stop_words.txt"
 	}
-	err = parser.StopWordsHandle.InitializeWordMap("../pkg/parser/utils/russian_stop_words.txt")
+
+	russian_stop_words_path := os.Getenv("RUSSIAN_STOP_WORDS_PATH")
+	if russian_stop_words_path == "" {
+		russian_stop_words_path = "../pkg/parser/utils/russian_stop_words.txt"
+	}
+
+	fmt.Println(russian_stop_words_path)
+
+	err := parser.StopWordsHandle.InitializeWordMap(english_stop_words_path)
 	if err != nil {
-		panic("Cannot scan stop-words")
+		log.Fatalf("Cannot scan stop-words: %v %s", err, english_stop_words_path)
+	}
+
+	err = parser.StopWordsHandle.InitializeWordMap(russian_stop_words_path)
+	if err != nil {
+		log.Fatalf("Cannot scan stop-words: %v %s", err, russian_stop_words_path)
 	}
 	// parser.StopWordsHandle.WordsMap.Range(func(key, value any) bool {
 	// 	fmt.Println(key)
