b38a863156b72ca4345802dbef67330a3ae6f378
fog66 <pol13112005@gmail.com>
2025-03-27 00:00:26 +0300

    Add config file and new CI components

--- go.mod
+++ go.mod
@@ -18,4 +18,5 @@ require (
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -2,25 +2,25 @@ package main
 
 import (
 	"log"
-	"os"
 
 	// "search-engine/pkg/indexer"
+
 	"search-engine/pkg/API"
+	"search-engine/pkg/config"
 	"search-engine/pkg/parser"
 )
 
 func main() {
-	english_stop_words_path := os.Getenv("ENGLISH_STOP_WORDS_PATH")
-	if english_stop_words_path == "" {
-		english_stop_words_path = "../pkg/parser/utils/english_stop_words.txt"
+	cnfg, err := config.GetConfig()
+	if err != nil {
+		log.Fatalf("Cannot read config")
 	}
 
-	russian_stop_words_path := os.Getenv("RUSSIAN_STOP_WORDS_PATH")
-	if russian_stop_words_path == "" {
-		russian_stop_words_path = "../pkg/parser/utils/russian_stop_words.txt"
-	}
+	english_stop_words_path := cnfg.SearchEngineConfig.EnglishStopWordsPath
 
-	err := parser.StopWordsHandle.InitializeWordMap(english_stop_words_path)
+	russian_stop_words_path := cnfg.SearchEngineConfig.RussianStopWordsPath
+
+	err = parser.StopWordsHandle.InitializeWordMap(english_stop_words_path)
 	if err != nil {
 		log.Fatalf("Cannot scan stop-words: %v %s", err, english_stop_words_path)
 	}
